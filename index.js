c75f676db4e0a5585ae41fa2c1d8e2f19561414c
Rafael Martins Machado <rafaelmartinsmachado25@gmail.com>
2025-01-26 13:17:09 -0300

    refactor-add-route-soma

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+//Rota
+    // Method HTTP / CRUD (CREATE, READ, UPDATE, DELETE)
+        //GET - Pega um info           READ
+        //POST - Cria uma info -       CREATE
+        //PUT - Altera toda a info     UPDATE
+        //PATH - Altera parte da info  UPDATE
+        //DELETE - Apaga uma info      DELETE
+
+    // Name - Um identificador da rota
+
+    // Function (Callback) - Responsável por executar algum comando
+
+app.get("/soma", (req, res) => {
+    const soma = 1 + 1;
+
+    res.send({soma: soma})
+});
 
 app.listen(3000)
